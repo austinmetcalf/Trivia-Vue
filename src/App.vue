7cb8300e77abbed3cab4a21a1bdb8198c180b1e6
austinmetcalf <austinchaz11@gmail.com>
2022-12-26 12:18:17 -0700

    Added vue components

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    
+    Austin is great usually
+    <button @click="getDataFromAPI">Get question</button>
+    <p>{{ currentQuestion }}test</p>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  // components: {
+  //   quizQuestion,
+  // },
+  methods: {
+    async getDataFromAPI() {
+      try {
+        const response = await axios.get('https://opentdb.com/api.php?amount=40&type=multiple');
+        this.dataFromAPI = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  data () {
+    return {
+      currentQuestion:'',
+    };
+  }, 
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/quizQuestion.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      
+     
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+ 
+  export default {
+    data() {
+      return {
+        questions: []
+      }
+    },
+    created() {
+      axios.get('https://opentdb.com/api.php?amount=40&type=multiple')
+        .then(response => {
+          this.questions = response.data.results;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+  </script>
+  
